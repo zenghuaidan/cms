68d8293b88d2c42dea1af89e6d96938dcd50d7c6
larry <larry.zeng@edeas.hk>
2018-01-02 10:55:13 +0800

    cms login filter

--- src/main/java/com/edeas/controller/AuthController.java
+++ src/main/java/com/edeas/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.edeas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -9,18 +10,20 @@ import com.edeas.model.User;
 @Controller
 public class AuthController extends CmsController {
 	
+	public static final String LOGIN_USER = "LOGIN_USER";
+	
 	@RequestMapping(path = {"login", ""}, method={RequestMethod.GET})
 	public String login(){
 		return "Auth/Login";
 	}
 	
 	@RequestMapping(path = {"login", ""}, method={RequestMethod.POST})
-	public String doLogin(String userName, String password) {
+	public String doLogin(Model model, String userName, String password) {
 		User user = userService.tryLogin(userName, password);
-		if(user != null) {
-			return "Auth/success";	
+		if(user == null) {
+			return "Auth/Login";	
 		}
-		return "Auth/fail";
+		return "SiteAdmin/Index";
 	}
 	
 }
---
+++ src/main/java/com/edeas/web/LoginFilter.java
@@ -0,0 +1,63 @@
+package com.edeas.web;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.edeas.controller.AuthController;
+import com.edeas.controller.CmsController;
+import com.edeas.model.User;
+
+public class LoginFilter implements Filter {
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest _request = (HttpServletRequest) request;   
+		HttpServletResponse _response = (HttpServletResponse) response;
+		
+		RequestMapping rm = CmsController.class.getAnnotation(RequestMapping.class);
+		String cmsurl = rm.value()[0];
+		
+		User user = (User)_request.getSession().getAttribute(AuthController.LOGIN_USER);
+		if (!isCmsLoginPage(cmsurl, _request) && user == null) {
+			_response.sendRedirect(_request.getContextPath() + cmsurl);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+	
+	public boolean isCmsLoginPage(String cmsurl, HttpServletRequest request) {
+		return isCmsLoginPage(new String[]{ cmsurl }, request);
+	}
+	
+	public boolean isCmsLoginPage(String[] values, HttpServletRequest request) {
+		for(String value : values) {
+			if (request.getRequestURI().toString().equals(request.getContextPath() + value) || request.getRequestURI().toString().equals(request.getContextPath() + value + "/")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// The InitServlet is not init yet, should use filterConfig to get the servletContext
+//		userService = (IUserService)WebApplicationContextUtils.getWebApplicationContext(filterConfig.getServletContext()).getBean("userService");
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
+
+}
